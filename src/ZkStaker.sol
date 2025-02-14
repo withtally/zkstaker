cdbbc3392c2a687e770eabb6e86954e9c357520c
John Feras <jferas@ferasinfotech.com>
2025-02-14 14:46:05 -0500

    Added staker contract and minimal tests

---
+++ src/ZkStaker.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.28;
+
+import {Staker, IERC20} from "staker/src/Staker.sol";
+import {StakerPermitAndStake} from "staker/src/extensions/StakerPermitAndStake.sol";
+import {StakerOnBehalf, EIP712} from "staker/src/extensions/StakerOnBehalf.sol";
+import {StakerDelegateSurrogateVotes} from "staker/src/extensions/StakerDelegateSurrogateVotes.sol";
+import {IERC20Staking} from "staker/src/interfaces/IERC20Staking.sol";
+import {IEarningPowerCalculator} from "staker/src/interfaces/IEarningPowerCalculator.sol";
+
+/// @title ZkStaker
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice A staking contract for ZkSync that extends the Staker contract from
+/// withtally/staker. This implementation includes permit functionality for gasless approvals,
+/// staking on behalf of other addresses, and delegation of voting power through surrogate
+/// contracts.
+/// @dev This contract combines multiple extension modules from the base Staker.
+contract ZkStaker is Staker, StakerPermitAndStake, StakerOnBehalf, StakerDelegateSurrogateVotes {
+  /// @notice Initializes the ZkStaker contract with required parameters
+  /// @param _rewardsToken ERC20 token in which rewards will be denominated
+  /// @param _stakeToken Delegable governance token which users will stake to earn rewards
+  /// @param _earningPowerCalculator The contract that will calculate earning power for stakers
+  /// @param _maxBumpTip Maximum tip that can be paid to bumpers for updating earning power
+  /// @param _admin Address which will have permission to manage reward notifiers
+  /// @param _name Name used in the EIP712 domain separator for permit functionality
+  constructor(
+    IERC20 _rewardsToken,
+    IERC20Staking _stakeToken,
+    IEarningPowerCalculator _earningPowerCalculator,
+    uint256 _maxBumpTip,
+    address _admin,
+    string memory _name
+  )
+    Staker(_rewardsToken, _stakeToken, _earningPowerCalculator, _maxBumpTip, _admin)
+    StakerPermitAndStake(_stakeToken)
+    StakerDelegateSurrogateVotes(_stakeToken)
+    EIP712(_name, "1")
+  {
+    MAX_CLAIM_FEE = 1e18;
+    _setClaimFeeParameters(ClaimFeeParameters({feeAmount: 0, feeCollector: address(0)}));
+  }
+}
---
+++ test/ZkStaker.t.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {ZkStaker, IERC20, IEarningPowerCalculator} from "src/ZkStaker.sol";
+import {IERC20Staking} from "staker/src/interfaces/IERC20Staking.sol";
+import {ERC20Fake} from "staker/test/fakes/ERC20Fake.sol";
+import {ERC20VotesMock} from "staker/test/mocks/MockERC20Votes.sol";
+import {MockFullEarningPowerCalculator} from "staker/test/mocks/MockFullEarningPowerCalculator.sol";
+
+contract ZkStakerTestBase is Test {
+  ERC20Fake rewardToken;
+  ERC20VotesMock govToken;
+  MockFullEarningPowerCalculator earningPowerCalculator;
+  ZkStaker zkStaker;
+
+  address admin;
+  uint256 maxBumpTip;
+  string name;
+
+  function setUp() public virtual {
+    rewardToken = new ERC20Fake();
+    govToken = new ERC20VotesMock();
+    earningPowerCalculator = new MockFullEarningPowerCalculator();
+    admin = makeAddr("admin");
+    maxBumpTip = 1e18;
+    name = "ZkStaker";
+  }
+
+  /// @dev Helper function to validate addresses for fuzz tests
+  function _validateAddresses(
+    address _rewardToken,
+    address _stakeToken,
+    address _earningPowerCalculator,
+    address _admin
+  ) internal pure {
+    // Ensure no zero addresses
+    vm.assume(_rewardToken != address(0));
+    vm.assume(_stakeToken != address(0));
+    vm.assume(_earningPowerCalculator != address(0));
+    vm.assume(_admin != address(0));
+
+    // Ensure addresses are different
+    vm.assume(_rewardToken != _stakeToken);
+    vm.assume(_rewardToken != _earningPowerCalculator);
+    vm.assume(_stakeToken != _earningPowerCalculator);
+  }
+}
+
+contract Constructor is ZkStakerTestBase {
+  function test_SetsInitializationParameters() public {
+    zkStaker = new ZkStaker(
+      IERC20(address(rewardToken)),
+      IERC20Staking(address(govToken)),
+      IEarningPowerCalculator(address(earningPowerCalculator)),
+      maxBumpTip,
+      admin,
+      name
+    );
+
+    assertEq(address(zkStaker.REWARD_TOKEN()), address(rewardToken));
+    assertEq(address(zkStaker.STAKE_TOKEN()), address(govToken));
+    assertEq(address(zkStaker.earningPowerCalculator()), address(earningPowerCalculator));
+    assertEq(zkStaker.maxBumpTip(), maxBumpTip);
+    assertEq(zkStaker.admin(), admin);
+  }
+
+  function testFuzz_SetsTheRewardTokenStakeTokenAndOwnerToArbitraryAddresses(
+    address _rewardToken,
+    address _stakeToken,
+    address _earningPowerCalculator,
+    uint256 _maxBumpTip,
+    address _admin,
+    string memory _name
+  ) public {
+    _validateAddresses(_rewardToken, _stakeToken, _earningPowerCalculator, _admin);
+
+    zkStaker = new ZkStaker(
+      IERC20(_rewardToken),
+      IERC20Staking(_stakeToken),
+      IEarningPowerCalculator(_earningPowerCalculator),
+      _maxBumpTip,
+      _admin,
+      _name
+    );
+
+    assertEq(address(zkStaker.REWARD_TOKEN()), _rewardToken);
+    assertEq(address(zkStaker.STAKE_TOKEN()), _stakeToken);
+    assertEq(address(zkStaker.earningPowerCalculator()), _earningPowerCalculator);
+    assertEq(zkStaker.maxBumpTip(), _maxBumpTip);
+    assertEq(zkStaker.admin(), _admin);
+  }
+
+  function testFuzz_SetsClaimFeeParameters(
+    address _rewardToken,
+    address _stakeToken,
+    address _earningPowerCalculator,
+    uint256 _maxBumpTip,
+    address _admin,
+    string memory _name
+  ) public {
+    _validateAddresses(_rewardToken, _stakeToken, _earningPowerCalculator, _admin);
+
+    zkStaker = new ZkStaker(
+      IERC20(_rewardToken),
+      IERC20Staking(_stakeToken),
+      IEarningPowerCalculator(_earningPowerCalculator),
+      _maxBumpTip,
+      _admin,
+      _name
+    );
+
+    assertEq(zkStaker.MAX_CLAIM_FEE(), 1e18);
+    (uint96 feeAmount, address feeCollector) = zkStaker.claimFeeParameters();
+    assertEq(feeAmount, 0);
+    assertEq(feeCollector, address(0));
+  }
+}
